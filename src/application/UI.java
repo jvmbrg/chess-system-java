fcecea5f64d82ee27972dd7a44251d238faf0c85
jvmbrg <jvitormbraga@gmail.com>
2024-06-27 19:19:30 -0300

    Printing possible moves

--- src/application/UI.java
+++ src/application/UI.java
@@ -58,17 +58,24 @@ public class UI {
 		for (int i = 0; i < pieces.length; i++) {
 			System.out.print((8 - i) + " ");
 			for (int j = 0; j < pieces.length; j++) {
-				printPiece(pieces[i][j]);
+				printPiece(pieces[i][j], false);
 			}
 			System.out.println();
 		}
 		System.out.println("  a b c d e f g h");
 	}
 
-	// Aqui é onde a peça do tabuleiro é impressa
-	private static void printPiece(ChessPiece piece) {
+	/* Aqui é onde a peça do tabuleiro é impressa. O parametro "background" em boolean, define se o programa vai ou não mostrar as jogadas 
+	 * possiveis para a peça selecionada durante a execução do metodo "printboard". 
+	 * 
+	 *  Por ser um parametro de matriz booleana, programa identifica se a peça tem naquele posição em que ela se encontra, alguma possibilidade
+	 *  de se locomover, se tiver, o programa marca em azul, senão, não exibe nada.*/
+	private static void printPiece(ChessPiece piece, boolean background) {
+		if(background) {
+			System.out.print(ANSI_BLUE_BACKGROUND);
+		}
     	if (piece == null) {
-            System.out.print("-");
+            System.out.print("-" + ANSI_RESET);
         }
         else {
             if (piece.getColor() == Color.WHITE) {
@@ -79,5 +86,17 @@ public class UI {
             }
         }
         System.out.print(" ");
+	}
+	
+	
+	public static void printBoard(ChessPiece[][] pieces, boolean [][] possibleMoves) {
+		for (int i = 0; i < pieces.length; i++) {
+			System.out.print((8 - i) + " ");
+			for (int j = 0; j < pieces.length; j++) {
+				printPiece(pieces[i][j], possibleMoves[i][j]);
+			}
+			System.out.println();
+		}
+		System.out.println("  a b c d e f g h");
 	}
 }
