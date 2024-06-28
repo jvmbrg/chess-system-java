d064c09b2fef21c56baef4c28ac4567bbff6f3b5
jvmbrg <jvitormbraga@gmail.com>
2024-06-28 10:46:54 -0300

    Piece move count

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -100,7 +100,8 @@ public class ChessMatch {
 	
 	//Responsavel por fazer o movimento das peças de xadrez
 	private Piece makeMove(Position source, Position target) {
-		Piece p = board.removePiece(source);
+		ChessPiece p = (ChessPiece)board.removePiece(source);
+		p.increaseMoveCount();
 		Piece capturedPiece = board.removePiece(target);
 		board.placePiece(p, target);
 		
